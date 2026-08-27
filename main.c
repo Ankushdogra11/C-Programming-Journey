d77ce3470e62a91fa50a759bfdb352d776797443
Ankushdogra11 <dograankush40@gmail.com>
2026-08-27 23:15:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello from C!\n");
-    printf("Edit main.c, then run `make run` to see your changes.\n");
-
-    return 0;
+    printf("Hello C!\n");
+   
+   return 0;
 }
