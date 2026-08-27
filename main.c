a469300cd08e9eb7ba357345402dd620ba9d68eb
Ankushdogra11 <dograankush40@gmail.com>
2026-08-27 23:13:44 +0530

    Fix C program output

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
 
 int main(void) {
-    printf("Hello C!\n");
+    printf("Hello from C!\n");
+    printf("Edit main.c, then run `make run` to see your changes.\n");
+
     return 0;
 }
