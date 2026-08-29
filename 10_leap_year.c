f711144924d27c1e16eb941c9edacbd317c1e3cb
Ankush-dogra <dograankush40@gmail.com>
2026-08-29 09:52:58 +0530

    Update 10_leap_year.c

--- 10_leap_year.c
+++ 10_leap_year.c
@@ -12,7 +12,7 @@ int year;
 printf("Enter year:\n");
 scanf("%d",&year);
 
-if(year % 400 == 0 || year % 4 ==0 && year % 100 != 0){
+if((year % 400 == 0) || (year % 4 ==0) && (year % 100 != 0)){
 	printf("%d is a leap year", year);
 	}
 else{
